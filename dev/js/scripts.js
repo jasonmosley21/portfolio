843c3b66cd00ec7c1dba8d1acbe008cc9af862d0
jasonmosley21 <jdm449@drexel.edu>
2023-08-10 09:58:36 -0400

    my gsap is actually working
    Fixed everything and started to add in some animations to the landing page

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,9 +1,29 @@
 import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { GSDevTools } from "gsap/GSDevTools";
 
-gsap.registerPlugin(GSDevTools);
+gsap.registerPlugin(ScrollTrigger, GSDevTools);
 
-gsap.to("#logo",{duration:1,x:100});
+function landingANIME(){
+    var tl = gsap.timeline();
+
+    tl.from("#logo",{duration:1,opacity:0, scale:.5})
+    tl.from("#landingtext", {duration:1, opacity:0,y:210})
+    return tl;
+}
+
+function landingscrollANIME(){
+    var tl = gsap.timeline({scrollTrigger:{trigger:"#landing", scrub:true, markers: true, end:"bottom 20%", start:"bottom 80%"}});
+
+    tl.to("#landing", {opacity: .5})
+    tl.from("#aboutme", {opacity:.5})
+    return tl;
+}
+
+var maintl = gsap.timeline();
+maintl.add(landingANIME())
+.add(landingscrollANIME())
+;
 
 
 
